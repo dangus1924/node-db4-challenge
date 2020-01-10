a39cb7c73102b7b2d95791a225af4df1b2081e7b
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-09 21:15:19 -0500

    created tables

--- data/migrations/20200108221135_initial.js
+++ data/migrations/20200108221135_initial.js
@@ -1,17 +1,38 @@
 
 exports.up = async function(knex) {
   await knex.schema.createTable('recipes', (table) => {
-      table.increment('id')
-      table.string('name')
-      table.string('instruction')
-      table.string('difficult_id')
+      table.increment('id').notNullable();
+      table.string('name').notNullable();        
+  })  
+  await knex.schema.createTable('ingredients', (table) => {
+      table.increment('id').notNullable();
+      table.string('name').notNullable();
+      table.string('quantity').notNullable();
   })
-  await knex.schema.createTable('difficult_Level', (table) => {
-      table.increment('id')
-      table.string('name')      
+  await knex.schema.createTable('recipe_ingredient', (table) => {
+    table.integer('recipe_id')
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    table.integer('ingredients_id')   
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    table.primary(['recipe_id', 'ingredients_id' ])
   })
-  await knex.schema.createTable('ingredients' (table) => {
-      table.increment('id')
+  await knex.schema.createTable('instruction', (table) => {
+    table.integer('recipe_id')
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    table.string('steps');
+    table.string('directions');    
   })
 };
 
